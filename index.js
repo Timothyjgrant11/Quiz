940255373c5bd7777f64d84cd94a6c8aee275e68
timothyjgrant11@gmail.com <timothyjgrant11@gmail.com>
2019-03-03 17:25:09 -0500

    changes

--- index.js
+++ index.js
@@ -8,7 +8,98 @@ const STORE = [ {
     ],
     correctAnswer: 2,
 
-}];
+},
+{
+    question: 'Which of the following is NOT a book by James Baldwin',
+    answers: [
+    'A - If Beale Street Could Talk',
+    'B - Notes of a Native Son',
+    'C The Fire Next to Me',
+    'D - The Color Purple',
+    ],
+    correctAnswer: 3,
+},
+{
+    question: 'What is the Address of the character Sherlock Holmes',
+    answers: [
+    'A - 42 Wallaby Way',
+    'B - 30 Rockefeller Plaza',
+    'C - 32 Windsor Gardens',
+    'D - 221B Baker Street',
+    ],
+    correctAnswer: 3,
+},
+{
+    question: 'Who is the narrator of the novel On The Road',
+    answers: [
+    'A - Neal Cassady',
+    'B - Sal Paradise',
+    'C - Dean Moriarty',
+    'D - Carlo Marx',
+    ],
+    correctAnswer: 1,
+},
+{
+    question: 'What year did George Orwell publish his book 1984',
+    answers: [
+    'A - 1949',
+    'B - 1982',
+    'C - 1984',
+    'D - 2002',
+    ],
+    correctAnswer: 0,
+},
+{
+    question: 'What day of the year does the book Ulysses take place?',
+    answers: [
+    'A - June 11th',
+    'B - March 1st',
+    'C - June 16th',
+    'D - November 5th',
+    ],
+    correctAnswer: 2,
+},
+{
+    question: 'Who wrote the novel Slaughterhouse Five?',
+    answers: [
+    'A - J.R.R. Tolkien',
+    'B - Aldous Huxley',
+    'C - George Orwell',
+    'D - Kurt Vonnegut',
+    ],
+    correctAnswer: 3,
+},
+{
+    question: 'Which novel opens and closes with the letters of Robert Walton?',
+    answers: [
+    'A - World of Walton',
+    'B - Dracula',
+    'C - Alice in Wonderland',
+    'D - Frankenstein',
+    ],
+    correctAnswer: 3,
+},
+{
+    question: 'Where does the novel the Outsiders take place?',
+    answers: [
+    'A - Dallas',
+    'B - Wichita',
+    'C - Tulsa',
+    'D - Kansas City',
+    ],
+    correctAnswer: 2,
+},
+{
+    question: "What was Mark Twain's real name?",
+    answers: [
+    'A - Tom Sawyer',
+    'B - Henry Rodgers',
+    'C - Samuel Clemens',
+    'D - Mark Twain',
+    ],
+    correctAnswer: 2,
+}
+];
 
 let questionNumber = 0;
 let score = 0;
@@ -83,7 +174,7 @@ function handleFeedbackPage(result) {
   var answerCorrectness;
   if (result) {
     score++;
-    answerCorrectness = 'Congrats! That is correct.';
+    answerCorrectness = 'Correct!';
   } else {
     answerCorrectness = 'Oops! Unfortunately incorrect.';
   }
@@ -107,7 +198,7 @@ function handleFinalPage() {
   var htmlString = '';
   htmlString += `<main role="main">
     <section class="results-page" role="main">
-      <h1>You scored ${score} / ${STORE.length}! ${finalAssessment(score)}</h1>
+      <h1>You scored ${score + 1} / ${STORE.length}! ${finalAssessment(score)}</h1>
     <a href="!!!link added here!!!">Take the quiz again</a>
     </section>
   </main>`;
